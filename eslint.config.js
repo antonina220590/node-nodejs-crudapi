65c3a4bb34b360c41159a4ee06520274c0376f5d
antonina220590 <tonya2290@gmail.com>
2025-05-11 20:31:43 +0300

    chore: setup eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"; // Для Prettier
+
+export default tseslint.config(
+  {
+    ignores: ["node_modules/", "dist/", "*.js", "eslint.config.js"],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2022,
+      },
+      parserOptions: {
+        project: true,
+
+        project: "./tsconfig.json",
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+      ],
+      "no-console": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+  eslintPluginPrettierRecommended
+);
